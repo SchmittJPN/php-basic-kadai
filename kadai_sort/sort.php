96e0c53eb0998fa0e3c81e61c21c43a6a8a8de95
SchmittJPN <185781100+SchmittJPN@users.noreply.github.com>
2025-03-15 00:01:35 +0900

    ソートの課題をアップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        function sort_2way(&$array, $order = true) {
+            usort($array, function($a, $b) use ($order) {
+                return $order ? ($a <=> $b) : ($b <=> $a);
+            });
+        }
+        ?>
+    </p>
+    <p>
+        <?php
+        $nums = [15, 4, 18, 23, 10 ];
+        echo '昇順にソートします。<br>';
+        sort_2way($nums, true);
+        foreach ($nums as $num) {
+            echo $num . '<br>';
+        }
+        echo '降順にソートします。<br>';
+        sort_2way($nums, false);
+        foreach ($nums as $num) {
+            echo $num . '<br>';
+        }
+        ?>
+    </p>
+</body>
+</html>
+        
+        
